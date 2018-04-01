4fcc0d6799c6959a6cb2a31a38b2dc1c21854a39
Mike North <michael.l.north@gmail.com>
2018-03-31 21:16:50 -0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+
+const o = new Observable<{ v: number }>(subscriber => {
+  let c = 0;
+  const task = setInterval(() => {
+    subscriber.next({ v: c++ });
+    if (c > 10) {
+      clearInterval(task);
+    }
+  }, 200);
+});
+
+o.pipe(
+  map<{ v: number }, number>(x => x.v * x.v),
+  map<number, number>(x => x * x),
+  take(3)
+).forEach(x => console.log(x));
