af3c7e2ffcafb317a0d003dda70c1aac5a8b6cef
Mike North <michael.l.north@gmail.com>
2018-04-05 11:36:14 -0700

    Generalize webpack config for building exercise and demo folders

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,59 @@
 // tslint:disable
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const titleize = require('titleize');
+
+const DEMOS = ['async-shortcomings', 'generator-function', 'compose-pipe'];
+const EXERCISES = ['pong'];
+
+const generateExampleInfo = (key, arr) => arr.map((item, idx) => {
+  const id = idx + 1;
+  return {
+    id,
+    menuLink: {
+      url: `/${key}/${id}-${item}`,
+      text: titleize(item.replace(/\-/g, ' '))
+    },
+    html: {
+      template: `./src/${key}/${id}-${item}/index.hbs`,
+      outfile: `./${key}/${id}-${item}/index.html`
+    },
+    entry: { name: `${key}/${id}-${item}/index`, file: `./src/${key}/${id}-${item}/index.ts` }
+  };
+}, {});
+
+const generateExampleEntries = arr =>
+  arr.reduce((acc, item, idx) => {
+    acc[item.entry.name] = item.entry.file;
+    return acc;
+  }, {});
+
+const generateHtmlPlugins = arr =>
+  arr.map(
+    (item, idx) =>
+      new HtmlWebpackPlugin({
+        inject: true,
+        filename: item.html.outfile,
+        chunks: [item.entry.name],
+        template: item.html.template
+      }),
+    {}
+  );
+
+const DEMO_INFO = generateExampleInfo('demo', DEMOS);
+const EXERCISE_INFO = generateExampleInfo('exercise', EXERCISES);
+
+const DEMO_ENTRIES = generateExampleEntries(DEMO_INFO);
+const EXERCISE_ENTRIES = generateExampleEntries(EXERCISE_INFO);
+
+const DEMO_HTML_PLUGINS = generateHtmlPlugins(DEMO_INFO);
+const EXERCISE_HTML_PLUGINS = generateHtmlPlugins(EXERCISE_INFO);
 
 module.exports = {
   entry: {
     index: './src/index.ts',
-    pong: './src/exercise/pong/index.ts',
-    'demo-async-shortcomings': './src/demo/1-async-shortcomings/index.ts'
+    ...EXERCISE_ENTRIES,
+    ...DEMO_ENTRIES
   },
   mode: 'development',
   module: {
@@ -31,27 +78,21 @@ module.exports = {
     ]
   },
   output: {
-    filename: '[name].js',
+    // filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
       chunks: ['index'],
+      templateParameters: {
+        demos: DEMO_INFO,
+        exercises: EXERCISE_INFO
+      },
       template: './src/index.hbs'
     }),
-    new HtmlWebpackPlugin({
-      inject: true,
-      filename: './exercise/pong/index.html',
-      chunks: ['pong'],
-      template: './src/exercise/pong/index.hbs'
-    }),
-    new HtmlWebpackPlugin({
-      inject: true,
-      filename: './demo/1-async-shortcomings/index.html',
-      chunks: ['demo-async-shortcomings'],
-      template: './src/demo/1-async-shortcomings/index.hbs'
-    })
+    ...DEMO_HTML_PLUGINS,
+    ...EXERCISE_HTML_PLUGINS,
   ],
   resolve: {
     // Add `.ts` and `.tsx` as a resolvable extension.
