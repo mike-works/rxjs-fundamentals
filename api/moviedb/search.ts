237cfdf583f903fe75bbbe3cf6b57e741388bf95
Mike North <michael.l.north@gmail.com>
2018-04-08 15:52:28 -0700

    Add random lag to search, to create likely races

--- api/moviedb/search.ts
+++ api/moviedb/search.ts
@@ -1,15 +1,28 @@
 import { Router as router } from 'express';
 import fetch from 'node-fetch';
 
+async function timeout(n: number) {
+  return new Promise(res => setTimeout(res, n));
+}
+
 export default function(apiKey, { apiBase }) {
   const r = router();
   r.get('/movie/:query', async (req, resp) => {
     const { query, page } = req.params;
-    let fetchResp = await fetch(
-      `${apiBase}/search/movie?api_key=${apiKey}&language=en-US&page=1&include_adult=false&query=${encodeURIComponent(
-        query
-      )}`
-    );
+    let cancelRequest = false;
+    let wait = 800 * Math.random();
+    await timeout(wait);
+    req.on('close', err => {
+      cancelRequest = true;
+    });
+    if (cancelRequest) {
+      return;
+    }
+    let url = `${apiBase}/search/movie?api_key=${apiKey}&language=en-US&page=1&include_adult=false&query=${encodeURIComponent(
+      query
+    )}`;
+    console.log('Movie query: ', query, ' ', ' after ', wait);
+    let fetchResp = await fetch(url);
     if (!fetchResp.ok) {
       resp.status(500);
       resp.json({ error: `${await fetchResp.text()}` });
@@ -20,11 +33,20 @@ export default function(apiKey, { apiBase }) {
 
   r.get('/tv/:query', async (req, resp) => {
     const { query, page } = req.params;
-    let fetchResp = await fetch(
-      `${apiBase}/search/tv?api_key=${apiKey}&language=en-US&page=1&include_adult=false&query=${encodeURIComponent(
-        query
-      )}`
-    );
+    let cancelRequest = false;
+    let wait = 800 * Math.random();
+    await timeout(wait);
+    req.on('close', err => {
+      cancelRequest = true;
+    });
+    if (cancelRequest) {
+      return;
+    }
+    let url = `${apiBase}/search/tv?api_key=${apiKey}&language=en-US&page=1&include_adult=false&query=${encodeURIComponent(
+      query
+    )}`;
+    console.log('TV query: ', query, ' ', ' after ', wait);
+    let fetchResp = await fetch(url);
     if (!fetchResp.ok) {
       resp.status(500);
       resp.json({ error: `${await fetchResp.text()}` });
@@ -35,11 +57,20 @@ export default function(apiKey, { apiBase }) {
 
   r.get('/person/:query', async (req, resp) => {
     const { query, page } = req.params;
-    let fetchResp = await fetch(
-      `${apiBase}/search/person?api_key=${apiKey}&language=en-US&page=1&include_adult=false&query=${encodeURIComponent(
-        query
-      )}`
-    );
+    let cancelRequest = false;
+    let wait = 800 * Math.random();
+    await timeout(wait);
+    req.on('close', err => {
+      cancelRequest = true;
+    });
+    if (cancelRequest) {
+      return;
+    }
+    let url = `${apiBase}/search/person?api_key=${apiKey}&language=en-US&page=1&include_adult=false&query=${encodeURIComponent(
+      query
+    )}`;
+    console.log('Person query: ', query, ' ', ' after ', wait);
+    let fetchResp = await fetch(url);
     if (!fetchResp.ok) {
       resp.status(500);
       resp.json({ error: `${await fetchResp.text()}` });
