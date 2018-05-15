250581d0759a5c1a885dab9e64bb9566f08153f5
Mike North <michael.l.north@gmail.com>
2018-05-15 07:34:34 -0700

    Ex5 solution

--- src/exercise/5-error-recovery/index.ts
+++ src/exercise/5-error-recovery/index.ts
@@ -8,24 +8,30 @@ import { unstableFibonacci } from './fixtures';
  * Retry up to 20 times
  */
 // TODO: Replace this with your solution
-unstableFibonacci().forEach(val =>
-  addLogPanelMessage('panel5a', `Value: ${val}`)
-);
+unstableFibonacci()
+  .pipe(retry(20))
+  .forEach(val => addLogPanelMessage('panel5a', `Value: ${val}`));
 
 /**
  * - Exercise 5.B
  * Retry if sequence was less than 5 digits
  */
 // TODO: Replace this with your solution
-unstableFibonacci().forEach(val =>
-  addLogPanelMessage('panel5b', `Value: ${val}`)
-);
+unstableFibonacci()
+  .pipe(take(5), retry())
+  .forEach(val => addLogPanelMessage('panel5b', `Value: ${val}`));
 
 /**
  * - Exercise 5.C
  * Retry no more than once per second
  */
 // TODO: Replace this with your solution
-unstableFibonacci().forEach(val =>
-  addLogPanelMessage('panel5c', `Value: ${val}`)
-);
+unstableFibonacci()
+  .pipe(
+    take(30),
+    retryWhen(errors => {
+      let lastFail: number | undefined;
+      return zip(errors, interval(1000));
+    })
+  )
+  .forEach(val => addLogPanelMessage('panel5c', `Value: ${val}`));
