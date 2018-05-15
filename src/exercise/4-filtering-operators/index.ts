e1e2418add4350459b8399c9800b8a398aca0c8d
Mike North <michael.l.north@gmail.com>
2018-05-15 07:01:10 -0700

    Ex4 solution

--- src/exercise/4-filtering-operators/index.ts
+++ src/exercise/4-filtering-operators/index.ts
@@ -8,4 +8,11 @@ import {
   submitSearchButton // <button> user clicks for running a search
 } from './fixtures';
 
-// TODO: implement your solution here
+fromEvent(submitSearchButton, 'click').subscribe(() => {
+  multiSearch(getSearchInputValue())
+    .pipe(filter(x => x.type === 'movie'), toArray())
+    .subscribe(item => {
+      clearResults();
+      setResults(item.map(i => `<li>${i.name || i.title} (${i.type})</li>`));
+    });
+});
