d08389a5ff94087b667ea8e7b8ad22e0e3e280d1
Mike North <michael.l.north@gmail.com>
2018-05-15 07:40:24 -0700

    Rollback exercise 6

--- src/exercise/6-aggregate-exercise/index.ts
+++ src/exercise/6-aggregate-exercise/index.ts
@@ -1,74 +1,15 @@
-import { fromEvent, ConnectableObservable, Subject } from 'rxjs';
 import { map, reduce, scan } from 'rxjs/operators';
-import './exercise';
 import {
-  exerciseSubject,
-  updateActivity,
-  updateCalories,
-  updateDistanceInFeet,
-  updateHeartrate,
-  updateTimeInSeconds,
-  ExerciseInfo
+  exerciseSubject, // Observable to monitor for exercise info
+  updateActivity, // Set the activity type in the UI
+  updateCalories, // Set the calories in the UI
+  updateDistanceInFeet, // Set the distance traveled in the UI
+  updateHeartrate, // Set the heartrate in the UI
+  updateTimeInSeconds, // Set the time elapsed in the UI
+  ExerciseInfo // interface for exercise data
 } from './exercise';
 import './styles';
 
-exerciseSubject
-  .pipe(
-    scan<ExerciseInfo, number>((acc, val) => {
-      return acc + val.distance;
-    }, 0)
-  )
-  .subscribe(d => {
-    updateDistanceInFeet(d);
-  });
-
-exerciseSubject.subscribe(ei => {
-  updateActivity(ei.name);
+exerciseSubject.subscribe(d => {
+  console.log('data: ', d);
 });
-
-exerciseSubject
-  .pipe(
-    scan<ExerciseInfo, number>((acc, val) => {
-      return acc + val.time;
-    }, 0)
-  )
-  .subscribe(t => {
-    updateTimeInSeconds(t / 1000);
-  });
-
-exerciseSubject
-  .pipe(
-    scan<ExerciseInfo, number>((acc, val) => {
-      return val.heartRate;
-    }, 0)
-  )
-  .subscribe(t => {
-    updateHeartrate(t);
-  });
-
-exerciseSubject
-  .pipe(
-    scan<ExerciseInfo, Array<{ calories: number; time: number }>>(
-      (acc, val) => {
-        let { calories, time } = val;
-        acc.push({ time, calories });
-        while (time - acc[0].time > 20000) {
-          acc.shift();
-        }
-        return acc;
-      },
-      [] as Array<{ calories: number; time: number }>
-    ),
-    map<Array<{ calories: number; time: number }>, { calories: number; time: number }>(arr =>
-      arr.reduce(
-        (acc, item) => {
-          return { calories: acc.calories + item.calories, time: acc.time + item.time };
-        },
-        { calories: 0, time: 0 }
-      )
-    ),
-    map(({ calories, time }) => calories / time * 1000 * 60 * 60)
-  )
-  .subscribe(c => {
-    updateCalories(c);
-  });
