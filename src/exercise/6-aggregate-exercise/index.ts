584075a516be5ee1bfb44fbd48a0ea23c7656012
Mike North <michael.l.north@gmail.com>
2018-05-15 07:42:05 -0700

    Ex6 solution

--- src/exercise/6-aggregate-exercise/index.ts
+++ src/exercise/6-aggregate-exercise/index.ts
@@ -10,6 +10,69 @@ import {
 } from './exercise';
 import './styles';
 
-exerciseSubject.subscribe(d => {
-  console.log('data: ', d);
+exerciseSubject
+  .pipe(
+    scan<ExerciseInfo, number>((acc, val) => {
+      return acc + val.distance;
+    }, 0)
+  )
+  .subscribe(d => {
+    updateDistanceInFeet(d);
+  });
+
+exerciseSubject.subscribe(ei => {
+  updateActivity(ei.name);
 });
+
+exerciseSubject
+  .pipe(
+    scan<ExerciseInfo, number>((acc, val) => {
+      return acc + val.time;
+    }, 0)
+  )
+  .subscribe(t => {
+    updateTimeInSeconds(t / 1000);
+  });
+
+exerciseSubject
+  .pipe(
+    scan<ExerciseInfo, number>((acc, val) => {
+      return val.heartRate;
+    }, 0)
+  )
+  .subscribe(t => {
+    updateHeartrate(t);
+  });
+
+exerciseSubject
+  .pipe(
+    scan<ExerciseInfo, Array<{ calories: number; time: number }>>(
+      (acc, val) => {
+        let { calories, time } = val;
+        acc.push({ time, calories });
+        while (time - acc[0].time > 20000) {
+          acc.shift();
+        }
+        return acc;
+      },
+      [] as Array<{ calories: number; time: number }>
+    ),
+    map<
+      Array<{ calories: number; time: number }>,
+      { calories: number; time: number }
+    >(arr =>
+      arr.reduce(
+        (acc, item) => {
+          return {
+            calories: acc.calories + item.calories,
+            time: acc.time + item.time
+          };
+        },
+        { calories: 0, time: 0 }
+      )
+    ),
+    map(({ calories, time }) => calories / time * 1000 * 60 * 60)
+  )
+  .subscribe(c => {
+    updateCalories(c);
+  });
